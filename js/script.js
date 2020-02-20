20b41444ba0990c9503149bde727e5ef83f0fa7e
Elena <elenachavez36@gmail.com>
2020-02-19 19:25:56 -0600

    funcionando

--- js/script.js
+++ js/script.js
@@ -1,26 +1,29 @@
 //optener elementos principales del HTML
 var containerCell = document.getElementById("container-cell");
 var containerPiece = document.getElementById("container-piece");
+var dialogElement = document.getElementById("dialog");
 var selectedPiece = null;
 
+document.onkeypress = keypress;
 
 //llamar la funcion
 createBoard();
 createPieces();
 
 //definicion de la funcion
-function createCell(width, height){
+function createCell(width, height, position){
 
 	var cellElement = document.createElement("div");
 	cellElement.style.width = width;
 	cellElement.style.height = height;
 	cellElement.style.border = "1px solid black";
 	cellElement.style.backgroundColor = "##EB5E00";
+	cellElement.dataset.position = position;
 	cellElement.onclick = clickCell;
 	return cellElement;
 }
 
-//definicion de la funcion
+//definicion de la funcion          parametros en la funcion
 function createPiece(width, height, piece){
 
 	var cellElement = document.createElement("div"); //objeto - aqui guardamoa la pieza
@@ -34,6 +37,7 @@ function createPiece(width, height, piece){
 	pieceElement.height = height;
 	pieceElement.border = "1px solid black";
 	pieceElement.src = piece.image;
+	pieceElement.dataset.position = piece.position;
 	pieceElement.onclick = clickPiece;
 	//mandar la pieza a la celda - agregar elementos al documento
 	cellElement.appendChild(pieceElement);
@@ -48,7 +52,7 @@ function createBoard(){
 	width /= 4;
 	height /= 4;
 	for(var i=0; i<16; i++){
-		let cellElement = createCell(width, height);
+		let cellElement = createCell(width, height, i);
 		addCell(cellElement);
 	}
 }
@@ -102,9 +106,53 @@ function clickCell(e){
 	}
 }
 
+function keypress(ke){
+	if(ke.keyCode == 101 || ke.keyCode == 69){
+		let result = evaluateBoard();
+		showDialog(result);
+	}
 
+}
+                  //parametro
+function showDialog(result){
+	var imgElement =  dialogElement.children[0];
+	var textContent = dialogElement.children[1];
+	if(result){
+		imgElement.src ="";  //poner la url de la imagen para ganar
+		imgElement.width ="100";
+		imgElement.height = "500";
+		textContent.innerText ="Ganaste";
+	} else {
+		imgElement.src ="https://i.pinimg.com/736x/f4/4a/3e/f44a3ec3be36b1f2b0df3d232353d4c9.jpg"; // imagen de perdiste
+		imgElement.width ="500";
+		imgElement.height = "500";
+		textContent.innerText = "Perdiste";
+		returnPieces();
+	}
+	dialogElement.style.display = "block";
+}
 
+function evaluateBoard(){
+	var cells = containerCell.children;         // se optendran los hijos  para guardarlos en cells
+	for(cell of cells){
+		let piece = cell.children[0];
+		if(piece.dataset.position != cell.dataset.position){
+			return false;
+		}
+	}
+	return true;
+}
 
+function returnPieces(){
+	let cells = containerCell.children;
+	let cellPieces = containerPiece.children;
+
+	for(cell of cells){
+		let position = cell.dataset.position;
+		let piece = cell.children[0];
+		cellPieces[position].appendChild(piece);
+	}           //[piece.dataset.position]   te regresa las fichas acomodadas
+}
 
 
 
